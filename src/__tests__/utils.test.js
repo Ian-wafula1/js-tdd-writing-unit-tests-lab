9c9ce2d1ce9bcb522a8f2f8e95a11c783998375b
Ian Wafula <neocorpse@proton.me>
2025-04-17 09:28:32 +0300

    Write tests

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,33 @@
 // Your tests here
+// Your code here
+// Should check if the word is spelled the same forwards and backwards and return a boolean
+
+import { isPalindrome } from '../utils.js';
+
+describe('isPalindrome', () => {
+	it('returns true when the word is the same forwards and backwards.', () => {
+		const word = 'racecar';
+		expect(isPalindrome(word)).toBe(true);
+	});
+	it('returns false when the word is different forwards and backwards.', () => {
+		const word = 'baby';
+		expect(isPalindrome(word)).toBe(false);
+	});
+	it('returns true for a combination of uppercase and lowercase letters.', () => {
+		const word = 'RAcEcaR';
+		expect(isPalindrome(word)).toBe(true);
+	});
+	it('returns false for an empty string', () => {
+		const word = '';
+		expect(isPalindrome(word)).toBe(false);
+	});
+	it('throws an error if the input has any non-alphabetic characters', () => {
+		const word = 'rac3car';
+		expect(() => isPalindrome(word)).toThrow();
+	});
+	it("throws an error if the input isn't a string", () => {
+		const word = 23;
+		expect(() => isPalindrome(word)).toThrow();
+	});
+});
+
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,10 @@
-// Your code here
+export function isPalindrome(word) {
+    if (word === '') {
+        return false;
+    }
+    if (typeof word !== 'string' || !/^[A-Za-z]+$/.test(word)) {
+        throw new Error('Input must be a string containing only alphabetic characters.');
+    }
+    word = word.toLowerCase()
+    return word === word.split('').reverse().join('')
+}
